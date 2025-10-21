7ea652c643d5f1ad914b94f6ad8f30fcfb77aa35
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-21 20:09:31 +0530

    applied swagger annotaion on main() method's class

--- src/main/java/com/nareshIt/SpringBootProjectEcOnlineBookStoreApplication.java
+++ src/main/java/com/nareshIt/SpringBootProjectEcOnlineBookStoreApplication.java
@@ -3,6 +3,17 @@ package com.nareshIt;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+        info = @Info(
+        title = "Ecommerc Book Store Management",
+        version = "1.0",
+        description = "Spring Boot E-Commerce Book Store Management",
+        contact = @Contact(name ="Naresh I Technology",email = "bhabani@gmail.com")))
+
 @SpringBootApplication
 public class SpringBootProjectEcOnlineBookStoreApplication {
 
