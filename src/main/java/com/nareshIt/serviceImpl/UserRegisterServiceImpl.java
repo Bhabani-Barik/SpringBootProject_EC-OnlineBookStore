490c9192f29e492d865ce58c1da5b01d7f30794e
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-21 20:19:35 +0530

    developed business logics for register new user into application

--- src/main/java/com/nareshIt/serviceImpl/UserRegisterServiceImpl.java
+++ src/main/java/com/nareshIt/serviceImpl/UserRegisterServiceImpl.java
@@ -1,22 +1,38 @@
 package com.nareshIt.serviceImpl;
 
+import java.util.Base64;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import com.nareshIt.entity.UserRegister;
+import com.nareshIt.model.UserRequestDto;
 import com.nareshIt.repository.UserRegisterRepo;
 import com.nareshIt.service.UserRegisterService;
 
-@Component
+@Service
 public class UserRegisterServiceImpl implements UserRegisterService {
 
 	@Autowired
 	UserRegisterRepo userRegisterRepo;
 
 	@Override
-	public String insertUserRegister(UserRegister userRegister) {
-		userRegisterRepo.save(userRegister);
-		return "save successfully";
+	public UserRegister insertUserRegister(UserRequestDto userRequestDto) {
+		
+		UserRegister user = new UserRegister();
+		
+		try {
+			user.setFirstName(userRequestDto.getFirstName());
+			user.setLastName(userRequestDto.getLastName());
+			user.setEmail(userRequestDto.getEmail());
+			user.setPassword(Base64.getEncoder().encodeToString(userRequestDto.getPassword().getBytes()));
+			user.setContactId(userRequestDto.getContactId());
+			userRegisterRepo.save(user);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return user;
+		
 	}
 
 }
