7b2fedbebf5b21a1a97aa92fed7a4c1dca8d1d86
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-21 16:39:25 +0530

    Develop userregisters api with ResponseEntity and Constants

--- src/main/java/com/nareshIt/controller/UserRegisterController.java
+++ src/main/java/com/nareshIt/controller/UserRegisterController.java
@@ -1,26 +1,33 @@
 package com.nareshIt.controller;
 
+import java.net.HttpURLConnection;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.nareshIt.entity.UserRegister;
+import com.nareshIt.model.ResponseMessage;
 import com.nareshIt.service.UserRegisterService;
+import com.nareshIt.utility.Constants;
 
 @RestController
 @RequestMapping("api")
 public class UserRegisterController {
-	
-	@Autowired UserRegisterService userRegisterService;
-	
-	@PostMapping("/userregister")
-	public String createUserRegister(@RequestBody UserRegister userRegister) {
-		
-		userRegisterService.insertUserRegister(userRegister);
-		
-		return "User register saved successfully";
+
+	@Autowired
+	UserRegisterService userRegisterService;
+
+	@PostMapping("/userregisters")
+	public ResponseEntity<ResponseMessage> createUserRegister(@RequestBody UserRegister userRegister) {
+
+		String insertUserRegister = userRegisterService.insertUserRegister(userRegister);
+
+		return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_CREATED, Constants.SUCCESS,
+				"user registered successfully", insertUserRegister));
 	}
 
 }
