018f4085402780ea81dbf03f1b83af3e5f9d8948
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-22 00:12:45 +0530

    Developed business logics for  getUserRegisterDetails and checkUserDetails method

--- src/main/java/com/nareshIt/service/UserRegisterService.java
+++ src/main/java/com/nareshIt/service/UserRegisterService.java
@@ -1,10 +1,15 @@
 package com.nareshIt.service;
 
 import com.nareshIt.entity.UserRegister;
+import com.nareshIt.model.UserRequest;
 import com.nareshIt.model.UserRequestDto;
 
 public interface UserRegisterService {
 
 	public UserRegister insertUserRegister(UserRequestDto userRequestDto);
+	
+	public UserRequest getUserRegisterDetails(Long  id);
+
+	public UserRegister checkUserDetails(UserRequestDto userRequestDto);
 
 }
--- src/main/java/com/nareshIt/serviceImpl/UserRegisterServiceImpl.java
+++ src/main/java/com/nareshIt/serviceImpl/UserRegisterServiceImpl.java
@@ -1,11 +1,13 @@
 package com.nareshIt.serviceImpl;
 
 import java.util.Base64;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nareshIt.entity.UserRegister;
+import com.nareshIt.model.UserRequest;
 import com.nareshIt.model.UserRequestDto;
 import com.nareshIt.repository.UserRegisterRepo;
 import com.nareshIt.service.UserRegisterService;
@@ -34,5 +36,41 @@ public class UserRegisterServiceImpl implements UserRegisterService {
 		return user;
 		
 	}
+	
+
+
+//	@Override
+//	public UserRegister checkUserDetails(UserRequestDto userRequestDto) {
+//		
+//		UserRegister findbyEmail = userRegisterRepo.findByEmail(userRequestDto.getEmail());
+//		
+//		//Decoding the Password
+//		if(findbyEmail!=null) {
+//			
+//			String decode = new String(Base64.getDecoder().decode(findbyEmail.getPassword()));
+//			
+//			if(decode.equals(userRequestDto.getPassword())) {
+//				
+//				return findbyEmail;
+//			}
+//			
+//		}
+//		return findbyEmail;
+//	}
+	
+	@Override
+	public UserRegister checkUserDetails(UserRequestDto userRequestDto) {
+		return Optional.ofNullable(userRegisterRepo.findByEmail(userRequestDto.getEmail())).filter(
+				user -> new String(Base64.getDecoder().decode(user.getPassword())).equals(userRequestDto.getPassword()))
+				.orElse(null);
+	}
+
+	@Override
+	public UserRequest getUserRegisterDetails(Long id) {
+		 Optional<UserRegister> byId = userRegisterRepo.findById(id);
+		UserRegister userRegister = byId.get();
+		return new UserRequest(userRegister.getFirstName(), userRegister.getLastName());
+	}
+
 
 }
