cd9f75f9001f5e11f3e8bae3346a60e19d95d080
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-23 16:20:56 +0530

    Developed customer controller

---
+++ src/main/java/com/nareshIt/controller/CustomerController.java
@@ -0,0 +1,138 @@
+package com.nareshIt.controller;
+
+import java.net.HttpURLConnection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nareshIt.entity.Customer;
+import com.nareshIt.model.ResponseMessage;
+import com.nareshIt.service.CustomerService;
+import com.nareshIt.utility.Constants;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+
+@RestController
+@RequestMapping("/api")
+public class CustomerController {
+
+	@Autowired CustomerService custmerService;
+	
+
+	 @Operation(summary = "Create User Custmers",description = "e commerece online books store  register the users")
+	    @ApiResponses({
+	     @ApiResponse(responseCode = "201",description = "user register successfully"),
+	     @ApiResponse(responseCode = "400",description = "user register failure"),
+	     @ApiResponse(responseCode = "500",description = "Internal server error")
+	     })
+	@PostMapping("/custmersave")
+		public ResponseEntity<ResponseMessage> createCustmers(@RequestBody Customer customer) {
+
+			try {
+				if (customer.getEmail() == null || customer.getEmail().isEmpty() || customer.getName() == null
+						|| customer.getName().isEmpty()) {
+
+					return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(
+							HttpURLConnection.HTTP_BAD_REQUEST, Constants.FAILED, "email and name cannot be empty"));
+				}
+
+				Customer insertCustmers = custmerService.insertCustmers(customer);
+
+				if (insertCustmers != null) {
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_CREATED, Constants.SUCCESS,
+							"custmer save successfully", insertCustmers));
+				} else {
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_BAD_REQUEST, Constants.FAILED,
+							"custmer Failed", insertCustmers));
+
+				}
+
+			} catch (Exception e) {
+				return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_INTERNAL_ERROR, Constants.FAILED,
+						"Internal server error"));
+			}
+		}
+	 
+	    @PutMapping("/custmerupadte")
+		public ResponseEntity<ResponseMessage> custmerUpdates(@RequestBody Customer customer) {
+
+			try {
+				if (customer.getEmail() == null || customer.getEmail().isEmpty() || customer.getName() == null
+						|| customer.getName().isEmpty()) {
+
+					return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(
+							HttpURLConnection.HTTP_BAD_REQUEST, Constants.FAILED, "email and name cannot be empty"));
+				}
+				if (customer.getId() == null) {
+
+					Customer insertCustmers = custmerService.UpdatessCustmers(customer);
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_CREATED, Constants.SUCCESS,
+							"custmer update successfully", insertCustmers));
+
+				} else {
+					Customer insertCustmers = custmerService.insertCustmers(customer);
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_OK, Constants.SUCCESS,
+							"custmer updated successfully", insertCustmers));
+
+				}
+			} catch (Exception e) {
+				return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_INTERNAL_ERROR, Constants.FAILED,
+						"Internal server error"));
+			}
+		}
+	 
+	 
+	    @PostMapping("/custmersaveOrupdates")
+		public ResponseEntity<ResponseMessage> custmerORUpdates(@RequestBody Customer customer) {
+
+			try {
+				if (customer.getEmail() == null || customer.getEmail().isEmpty() || customer.getName() == null
+						|| customer.getName().isEmpty()) {
+
+					return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ResponseMessage(
+							HttpURLConnection.HTTP_BAD_REQUEST, Constants.FAILED, "email and name cannot be empty"));
+				}
+				if (customer.getId() == null) {
+
+					Customer createdOrUpdatessCustmers = custmerService.createdOrUpdatessCustmers(customer);
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_CREATED, Constants.SUCCESS,
+							"custmer saved successfully", createdOrUpdatessCustmers));
+
+				} else {
+					Customer createdOrUpdatessCustmers = custmerService.createdOrUpdatessCustmers(customer);
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_OK, Constants.SUCCESS,
+							"custmer updated successfully", createdOrUpdatessCustmers));
+
+				}
+			} catch (Exception e) {
+				return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_INTERNAL_ERROR, Constants.FAILED,
+						"Internal server error"));
+			}
+		}
+	  
+	 
+	        @GetMapping("/getByCustmerId/{id}")
+			public ResponseEntity<ResponseMessage> custmerORUpdates(@PathVariable Long id) {
+
+				Customer byCustmersId = custmerService.getByCustmersId(id);
+				if (byCustmersId != null) {
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_OK, Constants.SUCCESS,
+							"custmer id getting successfully", byCustmersId));
+
+				} else {
+					return ResponseEntity.ok(new ResponseMessage(HttpURLConnection.HTTP_BAD_REQUEST, Constants.FAILED,
+							"custmer id getting Failed", byCustmersId));
+
+				}
+			}
+}
