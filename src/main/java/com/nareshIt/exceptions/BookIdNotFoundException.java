3bd73b99c000d7615bf8108bb419f35099a66e6d
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-27 16:32:37 +0530

    created custom BookIdNotFoundException, if book id not found

---
+++ src/main/java/com/nareshIt/exceptions/BookIdNotFoundException.java
@@ -0,0 +1,12 @@
+package com.nareshIt.exceptions;
+
+public class BookIdNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public BookIdNotFoundException(String msg) {
+		super(msg);
+	}
+
+	
+}
