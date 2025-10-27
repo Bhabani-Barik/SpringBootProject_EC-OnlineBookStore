5b1913577f5519577a823e9548ae5da49584c451
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-27 16:33:52 +0530

    Handled GlobalException for BookIdNotFoundException

--- src/main/java/com/nareshIt/exceptions/RestGlobalExceptionHandle.java
+++ src/main/java/com/nareshIt/exceptions/RestGlobalExceptionHandle.java
@@ -5,9 +5,11 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
 import com.nareshIt.SpringBootProjectEcOnlineBookStoreApplication;
 import com.nareshIt.model.ErrorResponseMessage;
 import com.nareshIt.utility.Constants;
@@ -91,6 +93,16 @@ public class RestGlobalExceptionHandle {
 
 	}
 	
+	@ExceptionHandler(BookIdNotFoundException.class)
+	public ResponseEntity<Object> custmerHandleBookException(BookIdNotFoundException ex) {
+		List<String> details = new ArrayList<>();
+		details.add("Error : Book Id not found");
+		details.add("Detailed Message:" + ex.getLocalizedMessage());
+		details.add("Timestamp:" + System.currentTimeMillis());
+		ErrorResponseMessage error = new ErrorResponseMessage(HttpURLConnection.HTTP_BAD_REQUEST, Constants.FAILURE, "NOT-FOUND",details);
+		return new ResponseEntity<>(error, HttpStatus.BAD_REQUEST);
+
+	}
 	
 	
 	
