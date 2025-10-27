cbdcaec5933bf334d86c9fa06cc1e4d1b2c79290
Bhabani-Barik <bhabanishankar.barik21@gmail.com>
2025-10-27 16:38:44 +0530

    developed BookExcelFileRepository interface

---
+++ src/main/java/com/nareshIt/repository/BooksExcelFileRepository.java
@@ -0,0 +1,9 @@
+package com.nareshIt.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.nareshIt.entity.BooksExcelFileEntity;
+
+public interface BooksExcelFileRepository extends JpaRepository<BooksExcelFileEntity, Long>{
+
+}
